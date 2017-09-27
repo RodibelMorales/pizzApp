1291c6af8d3410afd1af7345f0a843685fc9d31d
Rodibel Morales <rmorales@topmre.com>
2017-09-27 15:52:31 -0500

    se añadio la validacion del formulario y mensajes de error

--- src/pages/registro-login/registro-login.ts
+++ src/pages/registro-login/registro-login.ts
@@ -1,13 +1,9 @@
-import { LoginPage } from './../login/login';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Authentication } from './../../servicios/authentication';
-/**
- * Generated class for the RegistroLoginPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { LoginPage } from './../login/login';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @IonicPage()
 @Component({
@@ -18,7 +14,12 @@ export class RegistroLoginPage {
   /*definimos variables para enviar el correo y password a firebase*/
   email :string;
   password :string;
-  constructor(public navCtrl: NavController, public navParams: NavParams,private auth: Authentication) {
+  validaFormRegistro : FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams,private angularAuth: AngularFireAuth,public formBuild:FormBuilder,public alertCtrl:AlertController) {
+    this.validaFormRegistro=this.formBuild.group({
+      email   :['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.pattern(/^[a-z0-9_-]{6,18}$/)]],
+    });
   }
 
   ionViewDidLoad() {
@@ -27,14 +28,20 @@ export class RegistroLoginPage {
   /*Funcion para registrarce con un email*/
   async crearCuentaEmail(){
     try{
-      const result = await this.auth.createUserWithEmailAndPassword(this.email, this.password);
-      if(result){
+      this.angularAuth.auth.createUserWithEmailAndPassword(this.email, this.password)
+      .then(data=>{
         this.navCtrl.pop();
-      }else{
-        alert("ocurrio un error, por favor intentalo nuevamente");
-      }
+      }).catch(error=>{
+        let alerts = this.alertCtrl.create({
+          title: 'Upps!',
+          subTitle: 'Algo salio mal, intentalo nuevamente',
+          buttons: ['OK']
+        });
+        alerts.present();   
+      });
     }catch(e){
       console.error(e);
     }
   }
+  
 }
