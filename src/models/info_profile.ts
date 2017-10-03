28985960325c4e35d25266d8c9b89e7e913e0a11
Rodibel Morales <rmorales@topmre.com>
2017-10-03 15:01:32 -0500

    se añadieron las variables para trabajar el perfil

--- src/models/info_profile.ts
+++ src/models/info_profile.ts
@@ -1,10 +1,11 @@
 export interface info_profile{
     img_profile    :string;
-    nombre:string;
+    nombre         :string;
     ap_paterno     :string;
     ap_materno     :string;
     telefono       :number;
     direccion      :string;
     reputacion     :string;
     referencia     :string;
+    email          :string;
 }
