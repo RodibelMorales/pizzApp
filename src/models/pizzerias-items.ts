25f28db50fbb09fbe42cbad1f76e9c572fc88be7
Rodibel Morales <rmorales@topmre.com>
2017-10-17 12:29:46 -0500

    varibles definidas para el listado de pizzas y productos

---
+++ src/models/pizzerias-items.ts
@@ -0,0 +1,16 @@
+export interface info_profile{
+    abre :string;
+    cierra :string;
+    direccion:string;
+    logo:string;
+    menu_pizzeria:string;
+        menu_pizzeria_Producto:string;
+            Producto_imagen:string;
+            Producto_ingredientes:string;
+            Producto_nombre:string;
+            Producto_precio:string;
+            Producto_promo:string;
+    nombre:string;
+    telefono:string;
+
+}
